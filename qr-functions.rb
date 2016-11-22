8015a4d876eeecddcf3e1d31a8258ed3c1d768ca
gnupinguin <gnupinguin@yahoo.com>
2016-11-22 07:09:24 +0300

    calculate some properties

--- qr-functions.rb
+++ qr-functions.rb
@@ -1,16 +1,38 @@
 require 'opencv'
 include OpenCV
 
-module qr-functions
-  def depth(contour_tree)
-    count = 0;
-    while contour_tree = contour_tree.v_next
-      count += 1;
-    end
-    return count;
+def depth(contour_tree)
+  count = 0;
+  while contour_tree = contour_tree.v_next
+    count += 1;
   end
+  return count;
+end
 
-  def distance(p1, p2)
-    Math.sqrt( (p1.x - p2.x)**2 + (p1.y - p2.y)**2 )
+def distance(p1, p2)
+  Math.sqrt( (p1.x - p2.x)**2 + (p1.y - p2.y)**2 )
+end
+
+def lineEquation(pL, pM, pJ)#pL, pM, pJ - points
+  a = -((pM.y - pL.y) / (pM.x - pL.x));
+	b = 1.0;
+	c = (((pM.y - pL.y) /(pM.x - pL.x)) * pL.x) - pL.y;
+
+  # Now that we have a, b, c from the equation ax + by + c, time to substitute (x,y) by values from the Point J
+
+	pdist = (a * pJ.x + (b * pJ.y) + c) / sqrt((a * a) + (b * b));
+	return pdist;
+end
+
+def lineSlope(pL, pM)#pL, pM - points
+  dx = pM.x - pL.x;
+  dy = pM.y - pL.y;
+
+  if dy != 0
+    alignement = 1;
+    return [(dy / dx), alignement];
+  else				# Make sure we are not dividing by zero; so use 'alignement' flag
+    alignement = 0;
+    return [0.0, alignement];
   end
 end
--- qr-image.rb
+++ qr-image.rb
@@ -1,15 +1,20 @@
 #!/usr/bin/env ruby
 require 'opencv'
 include OpenCV
-require 'qr-functions.rb'
+require './qr-functions.rb'
 
 image = nil
 begin
-  image = CvMat.load("sample.jpg", CV_LOAD_IMAGE_COLOR) # Read the file.
+  image = CvMat.load("s1.jpeg", CV_LOAD_IMAGE_COLOR) # Read the file.
 rescue
   puts 'Could not open or find the image.'
   exit
 end
+
+QR_NORTH = 0;
+QR_EAST = 1;
+QR_SOUTH = 2;
+QR_WEST = 3;
 
 gray  = image.BGR2GRAY
 bin = gray.threshold(0x44, 0xFF, :binary)
@@ -22,7 +27,7 @@ markers = [];
 content = contours.v_next;
 
 while content
-  if depth(content) > 1
+  if depth(content) >= 1
     markers.push content;
   end
   content = content.h_next;
@@ -37,7 +42,7 @@ markers.each { |e|
   massCenters << CvPoint.new(moments[-1].m10 / moments[-1].m00, moments[-1].m01 / moments[-1].m00)
 }
 
-circle_options = { :color => CvColor::Blue, :line_type => :aa, :thickness => -1 }
+circle_options = { :color => CvColor::Red, :line_type => :aa, :thickness => -1 }
 massCenters.each{|e|
   image.circle!(e, 3, circle_options);
 }
@@ -45,17 +50,51 @@ massCenters.each{|e|
 A, B, C = 0, 1, 2;
 
 AB = distance(massCenters[A], massCenters[B]);
-AC = distance(massCenters[A], massCenters[C]);
 BC = distance(massCenters[B], massCenters[C]);
+CA = distance(massCenters[C], massCenters[A]);
 
+outlier, median1, median2  = nil;
 
+if  AB > BC && AB > CA
+  outlier = C; median1=A; median2=B;
+elsif ( CA > AB && CA > BC )
+  outlier = B; median1=A; median2=C;
+elsif ( BC > AB && BC > CA )
+  outlier = A;  median1=B; median2=C;
+end
+
+top = outlier; # The obvious choice
+
+dist = lineEquation(massCenters[median1], massCenters[median2], massCenters[outlier]);	# Get the Perpendicular distance of the outlier from the longest side
+slope, align = lineSlope(massCenters[median1], massCenters[median2]); # Also calculate the slope of the longest side
+#
+# # Now that we have the orientation of the line formed median1 & median2 and we also have the position of the outlier w.r.t. the line
+# # Determine the 'right' and 'bottom' markers
+right, bottom, orientation = nil;
+if align == 0
+  bottom = median1;
+  right = median2;
+elsif slope < 0 && dist < 0
+  bottom = median1;
+  right = median2;
+  orientation = QR_NORTH;
+elsif slope > 0 && dist < 0
+  right = median1;
+  bottom = median2;
+  orientation = QR_EAST;
+elsif slope < 0 && dist > 0
+  right = median1;
+  bottom = median2;
+  orientation = QR_SOUTH;
+elsif slope > 0 && dist > 0
+  bottom = median1;
+  right = median2;
+  orientation = QR_WEST;
+end
+
+# To ensure any unintended values do not sneak up when QR code is not present
 
 window = GUI::Window.new('Display window') # Create a window for display.
-
-markers.each{ |e|
-  poly = e.approx(:accuracy => accuracy)
-  image.draw_contours!(poly, CvColor::Red, CvColor::Blue, 2, :thickness => 2, :line_type => :aa);
-}
-
+# image = image.resize(CvSize.new 21, 21);
 window.show(image) # Show our image inside it.
 GUI::wait_key # Wait for a keystroke in the window.
