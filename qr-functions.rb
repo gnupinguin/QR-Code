d0f39d82eac8bf293775ba8b13d4f5b8c0f36187
gnupinguin <gnupinguin@yahoo.com>
2016-11-27 23:41:54 +0300

    Fix bugs with binarization

--- qr-functions.rb
+++ qr-functions.rb
@@ -38,18 +38,18 @@ def getMarkers(binImage)
   contours = binImage.find_contours(:mode => OpenCV::CV_RETR_TREE, :method => OpenCV::CV_CHAIN_APPROX_SIMPLE);
 
   markers = [];
-  content = contours.v_next;#NEED TO DO SMART SEARCH!!!!
+  content = contours;#NEED TO DO SMART SEARCH!!!!
 
   while content
-    if depth(content) >= 1
+    if depth(content) >= 5
       markers << content;
     end
     content = content.h_next;
   end
-  # puts markers
   markers.size.times{|i|
     markers[i] = markers[i].approx_poly
   }
+
   return markers;
 end
 
@@ -92,8 +92,19 @@ end
 def rotateDiag(image)
   gray  = image.BGR2GRAY
   bin = gray.threshold(0x44, 0xFF, :binary)
-  markers = getMarkers(bin);
+  canny = gray.canny(50, 150)
+  markers = getMarkers(canny);
+
   massCenters = getMassCenters(markers);
+  # circle_options = { :color => CvColor::Red, :line_type => :aa, :thickness => -1 }
+  # massCenters.each{|e|
+  #   image.circle! e, 1, circle_options
+  # }
+  #
+  # markers.each { |e|
+  #   image.draw_contours!(e, CvColor::Red, CvColor::Green, 2,
+  #                            :thickness => 2, :line_type => :aa)
+  # }
 
   median1, median2, outlier = getLocationMarkers(markers, massCenters)#median1 on the left then median2
   slopeDiag, perpDiag = lineSlope(massCenters[median1], massCenters[median2]);
@@ -105,19 +116,31 @@ def rotateDiag(image)
 
   elsif slopeDiag != -1 # why -1?
     currentAngle = Math.atan(slopeDiag) * 180 / Math::PI;
-    angle = 45 - currentAngle;
+    currentAngle = currentAngle > 0 ? 180 - currentAngle : currentAngle;
+    angle =  45 - currentAngle;
     center = CvPoint.new image.width/2, image.height/2
     image = image.warp_affine(CvMat.rotation_matrix2D(center, angle, 1))
   end
+  # window1 = GUI::Window.new('Side Rotate window') # Create a window for display.
+  # window1.show(image) # Show our image inside it.
+  # GUI::wait_key
   return image;
 end
 
 def rotateSide(image)
   gray  = image.BGR2GRAY
   bin = gray.threshold(0x44, 0xFF, :binary)
-  markers = getMarkers(bin);
+  canny = gray.canny(50, 150)
+  markers = getMarkers(canny);
   massCenters = getMassCenters(markers);
   median1, median2, outlier = getLocationMarkers(markers, massCenters)
+  # image.draw_contours!(massCenters[outlier], CvColor::Red, CvColor::Green, 2,
+  #                         :thickness => 2, :line_type => :aa)
+  # circle_options = { :color => CvColor::Blue, :line_type => :aa, :thickness => -1 }
+  # image.circle! massCenters[median1], 4, circle_options
+  # window1 = GUI::Window.new('Side Rotate window') # Create a window for display.
+  # window1.show(image) # Show our image inside it.
+  # GUI::wait_key
 
   slopeSide1, perpSide1 = lineSlope(massCenters[outlier], massCenters[median1]);#slopeSide1 may be equal 0 and perpSide1 = 1
   slopeSide2, perpSide2 = lineSlope(massCenters[outlier], massCenters[median2]);#slopeSide2 may be equal 0
@@ -137,5 +160,19 @@ def rotateSide(image)
   center = CvPoint.new image.width/2, image.height/2
   image = image.warp_affine(CvMat.rotation_matrix2D(center, angle, 1))
   return image;
+end
+
+def getBinarySquareColor(num, rows, columns, countPixelsBlock,  bin)
+  row = num.div rows
+  col = num % columns
+  sum = 0;
+  countPixelsBlock.times{|i|
+    countPixelsBlock.times{|j|
+      # printf "#{bin.at(row*10 + i, col*10 + j).to_a.inject(0, :+).ceil/(255)} "
+      sum += bin.at(row*countPixelsBlock + i, col*countPixelsBlock + j).to_a.inject(0, :+).ceil/(255)
+    }
+  }
+
+  return (sum / 100.0).round
 
 end
--- qr-image.rb
+++ qr-image.rb
@@ -4,10 +4,9 @@ include OpenCV
 require './qr-functions.rb'
 
 image = nil
-imageName = "s2.png"
 dataOutName = "data.txt"
 begin
-  image = CvMat.load(imageName, CV_LOAD_IMAGE_COLOR) # Read the file.
+  image = CvMat.load(ARGV[0], CV_LOAD_IMAGE_COLOR) # Read the file.
 rescue
   puts 'Could not open or find the image.'
   exit
@@ -19,15 +18,18 @@ image = rotateDiag(image);
 # window0 = GUI::Window.new('Diag Rotate window') # Create a window for display.
 # window0.show(image) # Show our image inside it.
 # GUI::wait_key
+image.save('3_step.jpg');
 image = rotateSide(image);
 # window1 = GUI::Window.new('Side Rotate window') # Create a window for display.
 # window1.show(image) # Show our image inside it.
 # GUI::wait_key
+image.save('4_step.jpg');
 
 gray  = image.BGR2GRAY
-bin = gray.threshold(0x44, 0xFF, :binary)
+bin = gray.threshold(127, 255, :binary)
+canny = gray.canny(50, 150)
 
-markers = getMarkers(bin);
+markers = getMarkers(canny);
 markers.sort!{|a,b|
   getMassCenter(a).x <=> getMassCenter(b).x
 }
@@ -36,6 +38,11 @@ if getMassCenter(markers[0]).y < getMassCenter(markers[1]).y#<   - because coord
 end
 
 O, L, M = markers;
+
+# image.circle!(O.min_area_rect2.points[1], 3, circle_options);
+window1 = GUI::Window.new('Side Rotate window') # Create a window for display.
+window1.show(image) # Show our image inside it.
+GUI::wait_key
 
 k, b = getLine(O.bounding_rect.bottom_left, O.bounding_rect.bottom_right);#lower bound
 
@@ -46,22 +53,47 @@ y = k * x + b;
 forthMarker = CvPoint.new x, y;
 
 
-width = (O.bounding_rect.bottom_left.x - forthMarker.x).abs;
-height = (forthMarker.y - L.bounding_rect.top_right.y).abs;
+puts width = (O.bounding_rect.bottom_left.x - forthMarker.x).abs;
+puts height = (forthMarker.y - L.bounding_rect.top_right.y).abs;
 
 rect = CvRect.new(L.bounding_rect.top_left.x, L.bounding_rect.top_left.y, width, height);
+bin = bin.sub_rect(rect);
+# window = GUI::Window.new('Cut Binary Image');
+# window.show(bin)
+# GUI::wait_key
 
-bin = image.sub_rect(rect);
-bin = bin.resize(CvSize.new(QR_STANDARD,QR_STANDARD))
+bin = bin.resize(CvSize.new(21,21))
+bin = bin.threshold(127, 255, :binary)
+
+bin.save('out.jpg');
+#Extract data from image to file
+# file = File.open(dataOutName, "w")
+
+# 21.size.times{|i|
+#   21.size.times{|j|
+#     # file.write "#{getBinarySquareColor(i*21 + j, bin)} "
+#     puts i*21 + j
+#
+#   }
+#   file.write "\n"
+# }
+# (21**2).times{|i|
+#   file.write "#{getBinarySquareColor(i, 21, 21, 10, bin)} "
+#   if i % 21 == 0 && i != 0
+#     file.write "\n"
+#   end
+# }
 
 #Extract data from image to file
 file = File.open(dataOutName, "w")
 bin.height.times{|i|
   bin.width.times{|j|
-    file.write "#{bin.at(i,j).to_a.inject(0, :+).ceil/(255*3)} "
+    file.write "#{bin.at(i,j).to_a.inject(0, :+).ceil/(255)}"
   }
   file.write "\n"
 }
+
+# getBinarySquareColor(21**2 - 2, 21, 21, 10, bin)
 
 
 window = GUI::Window.new('Display window')
